da11ded87c79140a3283b80a6153997d8221fb22
Riya1718 <riyajain6999@gmail.com>
2021-03-06 20:06:59 +0530

    commit

---
+++ Lec1_JS_Intro/function.js
@@ -0,0 +1,17 @@
+let sayHi=function(){
+    console.log("Hiii");
+}
+
+sayHi();
+console.log(sayHi)
+
+//console.log(sayHi());
+
+function sayHey(name){
+    console.log(name+ " says Heyyy");
+    return 10;
+}
+
+let value=sayHey("Riya");
+console.log(value);
+
