48dbdca52443817215d1f65184f1da9b7ac739e6
Riya_1718 <riyajain6999@gmail.com>
2021-06-19 21:32:59 +0530

    react

---
+++ Module3_React/movies/src/API/secrets.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = "579c7c5e87ed94bce84085bc0817b77a";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
+
+// example -> https://api.themoviedb.org/3/movie/550?api_key=579c7c5e87ed94bce84085bc0817b77a
