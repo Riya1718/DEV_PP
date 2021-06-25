f0bd67918040bd7924c081d07e406994b3cd0f89
Riya_1718 <riyajain6999@gmail.com>
2021-06-25 21:50:33 +0530

    hooks done

---
+++ Module3_React/hooks/src/App.js
@@ -0,0 +1,23 @@
+import UseState from "./Components/UseState";
+import UseEffect from "./Components/UseEffect";
+import HookTwo from "./Components/UseEffect";
+
+function App() {
+  return (
+    <div className="App">
+      <UseState></UseState>
+      {/* <HookTwo></HookTwo> */}
+    </div>
+  );
+}
+
+export default App;
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCg7q784yiRCFPN6QbUrg4coMA-6yWI8s8",
+//   authDomain: "login-app-4a0c3.firebaseapp.com",
+//   projectId: "login-app-4a0c3",
+//   storageBucket: "login-app-4a0c3.appspot.com",
+//   messagingSenderId: "980663952274",
+//   appId: "1:980663952274:web:706b50fd84949cff7a174b"
+// };
