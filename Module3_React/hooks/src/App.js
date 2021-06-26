01a6df8a1ee1dbb16104b5704d1c46846daf9a33
Riya_1718 <riyajain6999@gmail.com>
2021-06-26 18:58:51 +0530

    firebase done

--- Module3_React/hooks/src/App.js
+++ Module3_React/hooks/src/App.js
@@ -12,12 +12,3 @@ function App() {
 }
 
 export default App;
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCg7q784yiRCFPN6QbUrg4coMA-6yWI8s8",
-//   authDomain: "login-app-4a0c3.firebaseapp.com",
-//   projectId: "login-app-4a0c3",
-//   storageBucket: "login-app-4a0c3.appspot.com",
-//   messagingSenderId: "980663952274",
-//   appId: "1:980663952274:web:706b50fd84949cff7a174b"
-// };
